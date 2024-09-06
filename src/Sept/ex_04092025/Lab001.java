4ebbdec76ecec940a8458a6c46bdb1288760141c
ssarvadey <shalini.canfin@gmail.com>
2024-09-06 11:34:45 +0530

    First code by own

--- src/Sept/ex_04092025/Lab001.java
+++ src/Sept/ex_04092025/Lab001.java
@@ -1,4 +1,9 @@
 package Sept.ex_04092025;
 
 public class Lab001 {
+    public static void main (String[] args){
+        System.out.println("PSL Web");
+        System.out.println("PSL Web");
+        System.out.println("PSL Web");
+    }
 }
